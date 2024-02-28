80b546bba8a4056fe491d941fc680c1d30e45b38
CyanFaux <santafox26@gmail.com>
2024-02-27 20:34:52 -0500

    oh **** it works now part 2

--- src/components/MoleContainer.js
+++ src/components/MoleContainer.js
@@ -1,24 +1,31 @@
 // Just one example
-import { useState } from 'react'
-import Mole from './Mole'
-import EmptySlot from './EmptySlot'
+import { useState } from "react";
+import Mole from "./Mole";
+import EmptySlot from "./EmptySlot";
 
 //took from solution code & had chatGPT explain
 const MoleContainer = (props) => {
-    let [theMole, setTheMole] = useState(false)
+  let [theMole, setTheMole] = useState(false);
 
-    const handleClick = (e) => {
-        props.setScore(props.score + 1)
-        setTheMole(false)
-    }
+  const handleClick = (e) => {
+    props.setScore(props.score + 1);
+    setTheMole(false);
+  };
+  
+// ternary operator, huh
+  let displayMole = theMole ? (
+    <Mole
+      setScore={props.setScore}
+      toggle={setTheMole}
+      handleClick={handleClick}
+    />
+  ) : (
+    <EmptySlot toggle={setTheMole} />
+  );
 
-    let displayMole = theMole ? <Mole setScore={props.setScore} toggle={setTheMole} handleClick={handleClick} /> : <EmptySlot toggle={setTheMole} />
+  return (
+    <div style={{ display: "inline-block", width: "30vw" }}>{displayMole}</div>
+  );
+};
 
-    return (
-        <div style={{'display': 'inline-block', 'width': '30vw'}}>
-            {displayMole}
-        </div>
-    )
-}
-
-export default MoleContainer
+export default MoleContainer;
