9d3298986ae0913663ad44d46152dcc4d37e907e
CyanFaux <santafox26@gmail.com>
2024-02-27 19:22:13 -0500

    Initial commit

--- src/components/MoleContainer.js
+++ src/components/MoleContainer.js
@@ -4,7 +4,6 @@ import Mole from './Mole'
 function MoleContainer(props){
     return (
         <div>
-            <h2> Mole Container </h2>
             <Mole setScore={props.setScore} score={props.score}/>
         </div>
     )
